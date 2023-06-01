9bfd49f0c89f786e8ec67d57f97489e6a58bdd78
KimJiHeon <000224kjh@gmail.com>
2023-06-01 17:44:33 +0900

    자바의정석 공부

---
+++ src/main/java/CH4/EX4_12.java
@@ -0,0 +1,13 @@
+package CH4;
+
+public class EX4_12 {
+    public static void main(String[] args) {
+        for(int i=1; i<=5; i++)
+            System.out.println(i);
+
+        for(int i=1; i<=5; i++)
+            System.out.print(i);
+
+        System.out.println();
+    }
+}
---
+++ src/main/java/CH4/EX4_14.java
@@ -0,0 +1,8 @@
+package CH4;
+
+public class EX4_14 {
+    public static void main(String[] args) {
+        for(int i=1, j=10; i<=10; i++, j--)
+            System.out.printf("%d \t %d\n", i, j);
+    }
+}
