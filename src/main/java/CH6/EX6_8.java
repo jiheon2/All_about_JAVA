ff1d9fda8b1f269c890e7dfb35928c2fd1892d40
KimJiHeon <000224kjh@gmail.com>
2023-06-18 00:06:55 +0900

    자바의정석 공부

---
+++ src/main/java/CH6/EX6_8.java
@@ -0,0 +1,11 @@
+package CH6;
+
+import static CH6.EX6_7.firstMethod;
+
+public class EX6_8 {
+    public static void main(String[] args) {
+        System.out.println("main(String[] args)이 시작되었음.");
+        firstMethod();
+        System.out.println("main(String[] args)이 끝났음.");
+    }
+}
